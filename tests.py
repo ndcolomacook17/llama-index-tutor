eee2221fd277d385f452cfe03c7316059ece696d
Ian McLaughlin <ian.mclaughlin@prizeout.com>
2024-01-13 10:07:10 -0500

    Tests

--- tests.py
+++ tests.py
@@ -1,35 +1,35 @@
-import os
-
-os.environ["REPLICATE_API_TOKEN"] = "YOUR_REPLICATE_API_TOKEN"
-
-from llama_index.llms import Replicate
-
-llama2_7b_chat = "meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e"
-llm = Replicate(
-    model=llama2_7b_chat,
-    temperature=0.01,
-    additional_kwargs={"top_p": 1, "max_new_tokens": 300},
+from llama_index import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    ServiceContext,
 )
-
-# set tokenizer to match LLM
-from llama_index import set_global_tokenizer
-from transformers import AutoTokenizer
-
-set_global_tokenizer(
-    AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-chat-hf").encode
+from llama_index.llms import LlamaCPP
+from llama_index.llms.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
 )
+model_url = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin"
 
-from llama_index.embeddings import HuggingFaceEmbedding
-from llama_index import ServiceContext
-
-embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-service_context = ServiceContext.from_defaults(
-    llm=llm, embed_model=embed_model
-)
-
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
-
-documents = SimpleDirectoryReader("YOUR_DATA_DIRECTORY").load_data()
-index = VectorStoreIndex.from_documents(
-    documents, service_context=service_context
-)
+if __name__ == '__main__':
+    llm = LlamaCPP(
+        # You can pass in the URL to a GGML model to download it automatically
+        # model_url=model_url,
+        # optionally, you can set the path to a pre-downloaded model instead of model_url
+        model_path="/Users/ianmclaughlin/Downloads/llava-v1.5-7b-Q4_K.gguf",
+        # temperature=0.1,
+        # max_new_tokens=256,
+        # # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+        # context_window=3900,
+        # # kwargs to pass to __call__()
+        # generate_kwargs={},
+        # # kwargs to pass to __init__()
+        # # set to at least 1 to use GPU
+        # # model_kwargs={"n_gpu_layers": 1},
+        # # transform inputs into Llama2 format
+        # messages_to_prompt=messages_to_prompt,
+        # completion_to_prompt=completion_to_prompt,
+        # verbose=True,
+    )
+    response = llm.stream_complete("Give me a list of 10 hotdogs")
+    for r in response:
+        print(r.delta, end="", flush=True)
